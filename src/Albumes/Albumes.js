4a0b18c9acdce32486e4b64a803ce3c3f53426d2
407 <407@academicos.cesde.edu.co>
2022-10-20 14:36:56 -0500

    se crea albumes

--- src/Albumes/Albumes.js
+++ src/Albumes/Albumes.js
@@ -36,19 +36,28 @@ export function Albumes(){
 
         return(
             <>
-                <div className="row row-cols-1 row-cols-md-4 g-3 mt-3">
-                    {
-                    canciones.map(function(cancion){
-                                <div className="col mt-3">
-                                    <div className="card h-100 tarA">
-                                        <img src={cancion.album.images[0].url} className="img-fluid w-100 h-100" alt="foto"/>  
-                                        <audio src={cancion.preview_url} className="w-100" controls="controls"></audio>
-                                        <h1 className="text-center">{cancion.album.nombre}</h1>                             
+                <div className="fondoAlbumes">
+            <div className="row row-cols-1 row-cols-md-3 g-3 justify-content-center ">
+                {
+                    canciones.map((cancion) => {
+                        return (
+                            <>
+                                <div className="col mt-5 ">
+                                    <div className="card h-100 align-content-center border-5 border-dark">
+                                        <img src={cancion.album.images[0].url} className="img-fluid h-100 p-1 w-100" alt="foto" />
+                                        <h1 className="text-center  text-danger fs-3 mt-2">{cancion.name}</h1>
+                                        <h1 className="text-center  fs-3 text-info">{cancion.popularity}</h1>
+                                        <audio controls src={cancion.preview_url}></audio>
                                     </div>
                                 </div>
-                     })
+                            </>
+
+                        )
+
+                    })
                 }
-                </div>
+            </div>
+            </div>
             </>
         )
 
